e8a0d1f24a2a98705c07f574cf7d9e4a16127c35
sudhir <bhosalesudhir96@gmail.com>
2023-01-27 11:46:18 +0530

    Added DB Connection method

---
+++ DataBase/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumwithDotNetCore.DataBase
+{
+    public static class DBConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Dev"].ConnectionString);
+
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
