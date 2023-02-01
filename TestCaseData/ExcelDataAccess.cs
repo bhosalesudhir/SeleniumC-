82cbf1487c8d8bbecef206eda3f301096c024b25
sudhir <bhosalesudhir96@gmail.com>
2023-02-01 10:02:42 +0530

    Added method to read data from excel

---
+++ TestCaseData/ExcelDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using Dapper;
+/*
+1)OLE DB supports accessing data stored in any format (databases, spreadsheets, text files, and so on)
+2) add reference of Dapper , System.Data.oleDb package
+*/
+namespace SeleniumwithDotNetCore.TestCaseData
+{
+    public class ExcelDataAccess
+    {
+        public static string TestDataFileConnection()
+        {
+            /*
+             Data Provider: The first parameter or one of the parameters of a connection is the database provider. To specify the provider.  Microsoft.Jet.OLEDB.4.0 Or Microsoft.ACE.OLEDB.12.0
+
+             */
+            var fileName = ConfigurationManager.AppSettings["path"]; // path: excel file path 
+            var conn = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source = {0}; Extended Properties=Excel 12.0;", fileName);
+            return conn;
+        }
+
+        public static string GetTestData(string keyName)
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                connection.Open(); // dataset is sheetname of excel file , key name = testcasename
+                var query = string.Format("select * from [DataSet$] where key='{0}'", keyName);
+                var value = connection.Query<string>(query).FirstOrDefault();
+                connection.Close();
+                return value;
+            }
+        }
+    }
+}
+
